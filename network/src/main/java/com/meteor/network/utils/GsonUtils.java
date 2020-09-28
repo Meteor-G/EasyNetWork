d3fffb663663712fbfa3ce34a4ce52161e1fd71d
Gll <68001667@qq.com>
2020-09-29 00:18:56 +0800

    扩展Gson工具类

--- network/src/main/java/com/meteor/network/utils/GsonUtils.java
+++ network/src/main/java/com/meteor/network/utils/GsonUtils.java
@@ -2,6 +2,10 @@ package com.meteor.network.utils;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author GLL
@@ -31,5 +35,81 @@ public class GsonUtils {
                     .excludeFieldsWithoutExposeAnnotation()
                     .create();
         return mGson;
+    }
+
+    /**
+     * 转成json
+     *
+     * @param object
+     * @return
+     */
+    public String GsonString(Object object) {
+        String gsonString = null;
+        if (getGson() != null) {
+            gsonString = getGson().toJson(object);
+        }
+        return gsonString;
+    }
+
+    /**
+     * 转成bean
+     *
+     * @param gsonString
+     * @param cls
+     * @return
+     */
+    public <T> T GsonToBean(String gsonString, Class<T> cls) {
+        T t = null;
+        if (getGson() != null) {
+            t = getGson().fromJson(gsonString, cls);
+        }
+        return t;
+    }
+
+    /**
+     * 转成list
+     *
+     * @param gsonString
+     * @param cls
+     * @return
+     */
+    public <T> List<T> GsonToList(String gsonString, Class<T> cls) {
+        List<T> list = null;
+        if (getGson() != null) {
+            list = getGson().fromJson(gsonString, new TypeToken<List<T>>() {
+            }.getType());
+        }
+        return list;
+    }
+
+    /**
+     * 转成list中有map的
+     *
+     * @param gsonString
+     * @return
+     */
+    public <T> List<Map<String, T>> GsonToListMaps(String gsonString) {
+        List<Map<String, T>> list = null;
+        if (getGson() != null) {
+            list = getGson().fromJson(gsonString,
+                    new TypeToken<List<Map<String, T>>>() {
+                    }.getType());
+        }
+        return list;
+    }
+
+    /**
+     * 转成map的
+     *
+     * @param gsonString
+     * @return
+     */
+    public <T> Map<String, T> GsonToMaps(String gsonString) {
+        Map<String, T> map = null;
+        if (getGson() != null) {
+            map = getGson().fromJson(gsonString, new TypeToken<Map<String, T>>() {
+            }.getType());
+        }
+        return map;
     }
 }
